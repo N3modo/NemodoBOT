540639bca36408e2715ad37da0fae175d82a99e9
nemodo <anthony46.pailla@gmail.com>
2023-06-15 19:07:58 +0200

    Refactoring added possibility to change command prefix

--- command_vrac.py
+++ command_vrac.py
@@ -1,17 +1,16 @@
 import discord
+import os
 from discord.ext import commands
 import asyncio
 
-intents = discord.Intents().all()
-client = discord.Client(intents=intents)
-bot = commands.Bot(command_prefix='!', intents=intents)
+env_prefix = os.getenv("BOT_CMD_PREFIX")
+bot_prefix = env_prefix if env_prefix is not None else '!'
 
 
 def is_mod(ctx):
     return ctx.author.guild.permissions.manage_messages
 
 
-@bot.command()
 async def clear(ctx):
     message_count = len([msg async for msg in ctx.channel.history()])
     if message_count == 0:
@@ -23,3 +22,24 @@ async def clear(ctx):
         empty_message = await ctx.send('The channel is now empty!')
         await asyncio.sleep(3)  # wait for 3 seconds
         await empty_message.delete()
+
+
+async def command_prefix(this_bot: commands.Bot, message: discord.Message) -> str:
+    global bot_prefix
+    if not (not isinstance(message.channel, discord.channel.DMChannel)
+            and not isinstance(message.channel, discord.channel.GroupChannel)):
+        print(this_bot.user.name + " has received a message through DM")
+        return ''
+    return bot_prefix
+
+
+async def change_prefix(ctx, *, new_prefix: str):
+    global bot_prefix
+    p = new_prefix.split()
+    msg = "Prefix changed to : '" + p[0] + "'"
+    if len(p) == 1:
+        bot_prefix = p[0]
+    else:
+        bot_prefix = p
+        msg += "  (or '" + '\',\''.join(p[1:]) + "')"
+    await ctx.send(msg)
